dca959f9ddee22c04fe8616d63dfd9c6ddf30f18
DaneSlattery <bioplus28@gmail.com>
2018-02-25 22:40:53 +0200

    Cleaned up code before integration. (#5)
    * Created visualiser to show slices in a more visual manner.
    
    also created some more data set outputs
    
    * Score Calculator and Visualiser testing.
    
    * Cleaned up code before integration.

--- src/CallEverythingAtOnce.py
+++ src/CallEverythingAtOnce.py
@@ -3,19 +3,29 @@ from generate_output import GenerateOutput
 from visualiser import Visualiser
 from scorecalculator import ScoreCalc
 
-def main():
-    example = InputData('../datasets/small.in')
-    totalNumberOfCells = example.R*example.C
-# assume the slices piece is valid
-    slicedpizza = [7, [[0, 0, 1, 1], [0, 3, 1, 4], [0, 5, 1, 6],
-                       [2, 0, 3, 1], [2, 2, 3, 3], [2, 4, 3, 5],
-                       [4, 5, 5, 6]]]
 
-    seeme = Visualiser(example, slicedpizza[1])
-    seeme.printdata()
-    scorecalc = ScoreCalc(slicedpizza[1])
-    print('Score: ', scorecalc.score, ". Total Cells:", totalNumberOfCells)
-    output = GenerateOutput(slicedpizza, '../output/small.out')
+def main():
+# input data
+    inputDataFileName = '/home/dane/repos/oops/datasets/small.in'
+    print("Input Dataset: ", inputDataFileName)
+    smallData = InputData(inputDataFileName)
+
+# assume the slices piece is valid
+# magic code goes here
+    slicedSmallPizza = [7, [[0, 0, 1, 1], [0, 3, 1, 4], [0, 5, 1, 6],
+                            [2, 0, 3, 1], [2, 2, 3, 3], [2, 4, 3, 5],
+                            [4, 5, 5, 6]]]
+# visualise slices
+    Visualiser(smallData, slicedSmallPizza[1])
+
+# calculate the score
+    print('Score: ', ScoreCalc(slicedSmallPizza[1]).score)
+
+# output the the file
+    outputDataFileName = '/home/dane/repos/oops/output/small.out'
+    print("Output file: ", outputDataFileName)
+    GenerateOutput(slicedSmallPizza, outputDataFileName)
+
 
 if __name__ == '__main__':
     main()
